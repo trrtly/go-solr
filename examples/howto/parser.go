8ccab40c884dc100cca0deca30b63c89280da037
chenwenlong <chenwenlong@epweike.com>
2019-06-18 13:43:53 +0800

    refactor: 重构结果集获取方法

--- examples/howto/parser.go
+++ examples/howto/parser.go
@@ -3,18 +3,33 @@ package main
 import "github.com/trrtly/go-solr/solr"
 import "fmt"
 
+type result struct {
+	Response struct {
+		NumFound int `json:"numFound"`
+		Start    int `json:"start"`
+		Docs     []struct {
+			IndusName string `json:"indus_name"`
+		} `json:"docs"`
+	} `json:"response"`
+}
+
 func main() {
 
 	si, _ := solr.NewSolrInterface("http://localhost:8983/solr", "collection1")
 
-	r, err := si.Query().Q("title:add sucess 1").
-		Start(0).Rows(15).
+	var data result
+
+	si.Query().
+		FieldList("*").
+		Q("*").
+		FilterQuery("NOT indus_type:2").
+		FilterQuery("NOT g_id:0").
+		FilterQuery("indus_pid:0").
+		Sort("task_num desc").
+		Start(0).
+		Rows(1).
+		SetResult(&data).
 		Search().Result(nil)
 
-	if err != nil {
-		fmt.Println("Error when querying solr:", err.Error())
-		return
-	}
-
-	fmt.Println(r.Results.Docs)
+	fmt.Println(data.Response.Docs[0].IndusName)
 }
